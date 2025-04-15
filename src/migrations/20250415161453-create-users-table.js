11cadd863e7044145392f5b6998e03635106eb79
ThuanNTV <thuanntv721@gmail.com>
2025-04-15 23:18:52 +0700

    feat: add migration for Users table with necessary fields and indexes

---
+++ src/migrations/20250415161453-create-users-table.js
@@ -0,0 +1,62 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.ENUM('admin', 'staff', 'customer'),
+        defaultValue: 'customer',
+      },
+      isVerified: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      verificationToken: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      resetPasswordToken: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      resetPasswordExpires: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+
+    await queryInterface.addIndex('Users', ['email'], { unique: true });
+    await queryInterface.addIndex('Users', ['role']);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  },
+};
